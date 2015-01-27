2c02a533b40eb0d23432b52c4f9eb2c5fbd6d841
ajaropud <ajaropud@2975e042-1db5-4b82-a7e4-9367c78ffee0>
2015-01-27 13:33:09 +0000

    refs #232

---
+++ Backoffice/sources/bin/audit_run_scan.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mydir=`pwd`
+echo CONSOLE Mydir = $mydir
+
+HOST='localhost'
+DB='ngnms'
+USER='ngnms'
+PASSWD='ngnms'
+
+echo perl audit.pl  -d -s -L $HOST -D $DB -U $USER -W $PASSWD 
+/usr/bin/perl /home/ngnms/NGREADY/bin/audit.pl  -d -s -L $HOST -D $DB -U $USER -W $PASSWD
