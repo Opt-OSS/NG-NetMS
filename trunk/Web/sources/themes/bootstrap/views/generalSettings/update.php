3d3453f72e3f367dc4201c476af362d10087aa26
ajaropud <ajaropud@2975e042-1db5-4b82-a7e4-9367c78ffee0>
2015-02-23 10:51:16 +0000

    refs #257

--- trunk/Web/sources/themes/bootstrap/views/generalSettings/update.php
+++ trunk/Web/sources/themes/bootstrap/views/generalSettings/update.php
@@ -10,6 +10,6 @@ $this->breadcrumbs=array(
 	);*/
 	?>
 
-	<h1>Update GeneralSettings <?php echo $model->id; ?></h1>
+	<h1>Update <?php echo $model->label; ?></h1>
 
 <?php echo $this->renderPartial('_form',array('model'=>$model)); ?>
