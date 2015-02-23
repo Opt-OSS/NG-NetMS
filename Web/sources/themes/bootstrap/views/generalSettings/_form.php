a7b859ebb0d99332dc5470fc83f1074a0f83bede
ajaropud <ajaropud@2975e042-1db5-4b82-a7e4-9367c78ffee0>
2015-02-23 10:51:16 +0000

    refs #257

--- Web/sources/themes/bootstrap/views/generalSettings/_form.php
+++ Web/sources/themes/bootstrap/views/generalSettings/_form.php
@@ -9,8 +9,19 @@
 
     <?php echo $form->hiddenField($model,'name'); ?>
 	<?php echo $form->textFieldRow($model,'label',array('class'=>'span5','maxlength'=>100,"readonly"=>true)); ?>
-
+    <?php if($model->name != 'type access'){ ?>
 	<?php echo $form->textFieldRow($model,'value',array('class'=>'span5','maxlength'=>255)); ?>
+<?php
+    }
+    else
+    {
+        echo '<label for="GeneralSettings_value">Value of Attribute</label>';
+        echo $form->dropDownList($model,'value',
+        CHtml::listData(AccessType::model()->findAll(), 'name', 'name'),
+        array('empty'=>'Select here...','class'=>'span5'));
+    }
+?>
+
 
 <div class="form-actions">
 	<?php $this->widget('bootstrap.widgets.TbButton', array(
