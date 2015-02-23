4e546f590515eccff0fa6847b0b15baf4d792f36
ajaropud <ajaropud@2975e042-1db5-4b82-a7e4-9367c78ffee0>
2015-02-23 14:46:35 +0000

    refs #264

--- Web/sources/themes/bootstrap/views/layouts/main.php
+++ Web/sources/themes/bootstrap/views/layouts/main.php
@@ -762,7 +762,7 @@ $this->beginWidget('bootstrap.widgets.TbModal', array('id'=>'gpl3lic','htmlOptio
             Copyright &copy; <?php echo date('Y'); ?> by Opt/Net
         </div>
         <div style="margin-left:220px;float:left">
-            NG-NetMS v3.1
+            NG-NetMS v3.2
         </div>
         <div id="lic_id" onClick="viewLic()" style="margin-right:69px;float:right">
             Under license GPL-3.0
