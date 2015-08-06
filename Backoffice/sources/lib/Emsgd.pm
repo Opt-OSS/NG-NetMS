9e297b345063850291d6085951b8722e60b7f9b4
tmatsely <wfox70@users.noreply.github.com>
2015-08-06 20:19:49 +0000

    bug fixes in  3.4 release: - disabled debug output in archive.pl

--- Backoffice/sources/lib/Emsgd.pm
+++ Backoffice/sources/lib/Emsgd.pm
@@ -1,6 +1,25 @@
     use Data::Dumper;
 package Emsgd;
 
+sub pp {
+    my @text = shift;
+    my $backtrace = shift;
+    print "\n\n===debug V ----------------------------------------------------------";
+
+        my $count = 0;
+        {
+            my ($package, $filename, $line, $sub) = caller($count);
+            last unless defined $line;
+            print sprintf("\n%02i %5i %-35s %-20s", $count++, $line, $sub, $filename);
+                     last unless $backtrace ;
+            redo;
+        }
+    print "\n===\n";
+print Data::Dumper->Dump([@text],[qw(message)]);
+#        print "@text";
+    print "\n===debug end ^----------------------------------------------------------\n\n";
+}
+
 sub print {
     my @text = shift;
     my $backtrace = shift;
