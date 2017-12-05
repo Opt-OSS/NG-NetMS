c074e5f9cb1f10f5e3e8630d7d0b2caf69bba280
Taras Matselyukh <taras@opt-net.eu>
2017-12-05 18:52:40 +0100

    pushing last changes for release 3.70 - part II

--- Core/NGREADY/lib/NGNMS/Plugins/Core/Skeleton/test/fixtures/device/expect/getConfig.pl
+++
@@ -1,5 +0,0 @@
-use warnings FATAL => 'all';
-use strict;
-<<'HEREDOC_CONFIG'
-shold be implemented
-HEREDOC_CONFIG
--- Core/NGREADY/lib/NGNMS/Plugins/Core/Skeleton/test/fixtures/device/expect/getHostName.pl
+++
@@ -1,3 +0,0 @@
-use warnings FATAL => 'all';
-use strict;
-'should be implemented'
